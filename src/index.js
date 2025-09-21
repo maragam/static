85d207cf438e088220f64597d5e66c4da5988c35
Mirza Delic <mirza.delic@softwareone.com>
2025-09-21 17:41:06 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from "./App";
 
 const msalInstance = new PublicClientApplication({
   auth: {
-    clientId: "<frontend-client-id>", // replace with your frontend app registration ID
-    authority: "https://login.microsoftonline.com/<tenant-id>", // replace with your tenant ID
+    clientId: "c26d9097-fcc7-4ed1-b90d-52397cf29862", // replace with your frontend app registration ID
+    authority: "https://login.microsoftonline.com/17a865da-0315-41af-bacb-97e4174a440b", // replace with your tenant ID
     redirectUri: "http://localhost:3000", // or your deployed app URL
   },
 });
