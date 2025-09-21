8548a69d074c099fa3d05fdc48f255fa26a7ea9d
Mirza Delic <mirza.delic@softwareone.com>
2025-09-21 18:03:17 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const msalInstance = new PublicClientApplication({
   auth: {
     clientId: "c26d9097-fcc7-4ed1-b90d-52397cf29862", // replace with your frontend app registration ID
     authority: "https://login.microsoftonline.com/17a865da-0315-41af-bacb-97e4174a440b", // replace with your tenant ID
-    redirectUri: "http://localhost:3000", // or your deployed app URL
+    redirectUri: "https://nice-tree-05912f403.2.azurestaticapps.net", // or your deployed app URL
   },
 });
 
